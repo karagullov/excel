6eeb045231a956f7a9030e069b58e586bff20788
erlannn777 <erlank123456789@gmail.com>
2023-02-14 15:29:44 +0600

    dinamic create table rows

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,9 +3,9 @@ const CODES = {
   Z: 90,
 };
 
-function createCell() {
+function toCell(content) {
   return `
-        <div class="cell" contenteditable>B2</div>
+        <div class="cell" contenteditable>${content}</div>
     `;
 }
 
@@ -17,10 +17,10 @@ function toColumn(col) {
     `;
 }
 
-function createRow(content) {
+function createRow(number, content) {
   return `
         <div class="row">
-            <div class="row-info"></div>
+            <div class="row-info">${number || ""}</div>
             <div class="row-data">${content}</div>
         </div>
     `;
@@ -36,10 +36,11 @@ export function createTable(rowsCount = 15) {
 
   const cols = new Array(colsCount).fill("").map(toChar).map(toColumn).join("");
 
-  rows.push(createRow(cols));
+  rows.push(createRow(null, cols));
 
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow());
+    const cells = new Array(colsCount).fill("").map(toCell).join("");
+    rows.push(createRow(i + 1, cells));
   }
 
   return rows.join("");
