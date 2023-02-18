847e49e9bf57840ce260a524ea828213cc66843c
erlannn777 <erlank123456789@gmail.com>
2023-02-18 15:56:56 +0600

    All instances of the ExcelComponent class interact with the emitter via the ExcelComponent(Facade pattern)

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,20 +4,38 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.emitter = options.emitter;
+    this.unsubcribers = [];
     this.prepare();
   }
 
+  //Настраиваем наш компонент до init
   prepare() {}
 
   toHTML() {
     return "";
   }
 
+  //Увеомляем слушателей про событие event
+  $emit(eventName, ...args) {
+    this.emitter.emit(eventName, ...args);
+  }
+
+  //Подписываемся на событие event
+  $on(eventName, fn) {
+    const unsub = this.emitter.subscribe(eventName, fn);
+    this.unsubcribers.push(unsub);
+  }
+
+  //Инициализируем компонент
+  //Добавляем DOM слушателей
   init() {
     this.initDOMListeners();
   }
 
+  //Удалякм компонент
+  //Чистим слушатели
   destroy() {
     this.removeDOMListeners();
+    this.unsubcribers.forEach((unsub) => unsub());
   }
 }
