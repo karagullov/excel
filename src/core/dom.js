7e4a0e0086ce3cc1fa71dc4bacdf0acbbd6f183b
erlannn777 <erlank123456789@gmail.com>
2023-02-18 16:10:35 +0600

    The tabe and formula components are connected to the keydown event

--- src/core/dom.js
+++ src/core/dom.js
@@ -15,7 +15,15 @@ class Dom {
   }
 
   text(text) {
-    this.$el.textContent = text;
+    if (typeof text === "string") {
+      this.$el.textContent = text;
+      return this;
+    }
+    if (this.$el.tagName.toLowerCase() === "input") {
+      return this.$el.value.trim();
+    } else {
+      return this.$el.textContent.trim();
+    }
   }
 
   clear() {
