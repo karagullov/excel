f065dfbdbd65c75840a6e8188799336fad25dd66
erlannn777 <erlank123456789@gmail.com>
2023-02-19 14:06:06 +0600

    Save in the store the dimensions of the changed cols

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,3 +1,11 @@
 export function rootReducer(state, action) {
-  return state;
+  let prevColState;
+  switch (action.type) {
+    case "TABLE_RESIZE":
+      prevColState = state.colState || {};
+      prevColState[action.data.id] = action.data.size;
+      return { ...state, colState: prevColState }; //id, value
+    default:
+      return state;
+  }
 }
