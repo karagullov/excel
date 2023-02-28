2a92767a0b560c88405a8cc1e22992314b79791c
erlannn777 <erlank123456789@gmail.com>
2023-02-28 23:15:08 +0600

    Added link to excel page

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -3,12 +3,14 @@ import { Page } from "../core/Page";
 
 export class DashboardPage extends Page {
   getRoot() {
+    const now = Date.now().toString();
+
     return $.create("div", "db").html(`
     <div class="db__header"><h1>Excel Dashboard</h1></div>
 
     <div class="db__new">
       <div class="db__view">
-        <a href="#" class="db__create"> Новая <br />Таблица </a>
+        <a href="#excel/${now}" class="db__create"> Новая <br />Таблица </a>
       </div>
     </div>
 
