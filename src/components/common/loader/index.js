bc5f3c5955dcc21e0b88b136ed5f3faa2f83c392
erlannn777 <erlank123456789@gmail.com>
2023-03-03 00:29:31 +0600

    Implement DIP

---
+++ src/components/common/loader/index.js
@@ -0,0 +1,20 @@
+import { $ } from "../../../core/dom/dom";
+
+export function Loader() {
+  return $.create("div", "loader").html(`
+    <div class="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    `);
+}
